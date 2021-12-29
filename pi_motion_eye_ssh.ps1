28a1dab1f36b73b608fea20d7238304e899a4c69
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2021-12-29 06:07:42 -0800

    init

---
+++ pi_motion_eye_ssh.ps1
@@ -0,0 +1,5 @@
+# ssh-copy-id root@10.0.0.7
+
+Get-Content $env:USERPROFILE/.ssh/id_rsa.pub | ssh {root@10.0.0.7} "cat >> .ssh/authorized_keys"
+
+scp $env:USERPROFILE/.ssh/id_rsa.pub root@10.0.0.7:.ssh/authorized_keys
